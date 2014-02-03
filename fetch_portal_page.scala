15f1d076a8f61b0887598251abfccf4d6c6582c3
Ellen König <ellen@ellenkoenig.de>
2014-02-03 23:12:32 +0100

    First working version of basic crawler

--- fetch_portal_page.scala
+++ fetch_portal_page.scala
@@ -2,19 +2,19 @@ import scala.io._
 import scala.actors._
 import Actor._
 
-object  HomePageLoader {
+object HomePageLoader {
 	
 	val portalUrl = "http://de.wikipedia.org/wiki/Portal:Geist_und_Gehirn"
 
 	def fetchPortalPage() = {
 		val caller = self
 
-		actor { caller ! (portalUrl, Source.fromURL(portalUrl))}
+		actor { caller ! (portalUrl, Source.fromURL(portalUrl).mkString)}
 
 		receive {
 			case (portalUrl, text) =>
 				println(portalUrl)
-				println(text)
+				print(text)
 		}
 	}
 }
